2c74043349c66ecbc721af7e47013ac2bf0274bc
M Furkan Olcer <muhammedfurkanolcer@hotmail.com>
2024-03-25 23:39:12 +0300

    Veritabanı ayarları eklendi.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { OgrenciModule } from './ogrenci/ogrenci.module';
+import { BolumModule } from './bolum/bolum.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'simform',
+      username: 'postgres',
+      entities: [],
+      database: 'pgWithNest',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+    OgrenciModule,
+    BolumModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
