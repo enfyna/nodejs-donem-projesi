b81c3f1259caff56b74dcbc4087b0c11151a2a02
enfyna <legendraslan@gmail.com>
2024-03-27 00:14:44 +0300

    Ögrenci ve Bölümü entity dizisine ekle.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { OgrenciModule } from './ogrenci/ogrenci.module';
 import { BolumModule } from './bolum/bolum.module';
+import { Ogrenci } from './ogrenci/entities/ogrenci.entity';
+import { Bolum } from './bolum/entities/bolum.entity';
+// import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -12,14 +14,13 @@ import { BolumModule } from './bolum/bolum.module';
       type: 'postgres',
       host: 'localhost',
       port: 5432,
-      password: 'simform',
-      username: 'postgres',
-      entities: [],
-      database: 'pgWithNest',
+      password: '',
+      username: 'admin',
+      entities: [Ogrenci, Bolum],
+      database: 'nodejs_proje_db',
       synchronize: true,
       logging: true,
     }),
-    UserModule,
     OgrenciModule,
     BolumModule,
   ],
