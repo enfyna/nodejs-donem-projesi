ea2a1a29424c74ce297b6403a7558381544fd99b
enfyna <legendraslan@gmail.com>
2024-03-27 00:30:05 +0300

    Öğrenci modülüne Bölümü tanımla.

--- src/ogrenci/ogrenci.module.ts
+++ src/ogrenci/ogrenci.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { OgrenciService } from './ogrenci.service';
 import { OgrenciController } from './ogrenci.controller';
+import { Ogrenci } from './entities/ogrenci.entity';
+import { Bolum } from 'src/bolum/entities/bolum.entity';
+import { BolumService } from 'src/bolum/bolum.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Ogrenci, Bolum])],
   controllers: [OgrenciController],
-  providers: [OgrenciService],
+  providers: [OgrenciService, BolumService],
 })
+
 export class OgrenciModule {}
